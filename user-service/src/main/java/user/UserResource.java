d6431c73dfc80516502a8d0628ee3dd03e5206eb
itayventura <ventura.itay@gmail.com>
2023-02-24 12:44:06 +0200

    injecting logger, logging user by id request

--- user-service/src/main/java/user/UserResource.java
+++ user-service/src/main/java/user/UserResource.java
@@ -1,8 +1,9 @@
 package user;
 
 import io.smallrye.mutiny.Uni;
-//import org.jboss.logging.Logger;
+import org.jboss.logging.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,12 +13,18 @@ import javax.ws.rs.core.MediaType;
 @Path("/users")
 public class UserResource {
 
-//    Logger logger;
+    final Logger logger;
+
+    @Inject
+    public UserResource(Logger logger) {
+        this.logger = logger;
+    }
 
     @Path("/{userId}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<User> getUserById(@PathParam("userId") String userId){
+        logger.infof("get user %s by id", userId);
         return Uni.createFrom().item(new User(userId));
     }
 }
